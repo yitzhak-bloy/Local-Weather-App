b70ff048ff2c3f9f7b6df843fdc3f20b4082a691
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-04-25 22:36:36 +0300

    Add a fetch method

--- src/ShowingWeather.js
+++ src/ShowingWeather.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ShowingWeather = () => {
+
+  const [api, setApi] = useState('');
+
+    fetch('https://fcc-weather-api.glitch.me/api/current?lat=31.893429&lon=34.813946')
+        .then(response => response.json())
+        .then((user) =>{
+          //  console.log(user)  
+           setApi(user)
+          })
+
     return (
         <div>
             <h1>cold</h1>
+            <p>{api.name}</p>
         </div>
     )
 }
