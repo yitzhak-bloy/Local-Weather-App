a8a3176635c609e9929faf6729d4568350b1bd4f
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-05-02 21:19:55 +0300

    Updates

---
+++ precache-manifest.0dfb93fca7404eeb46630c1c424ea576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c7eb0e306584ffa042d46185ec5dc3",
+    "url": "/Local-Weather-App/index.html"
+  },
+  {
+    "revision": "cf54ae682f8792a0e040",
+    "url": "/Local-Weather-App/static/css/main.5edd50e4.chunk.css"
+  },
+  {
+    "revision": "10ac61f8eda1a117771a",
+    "url": "/Local-Weather-App/static/js/2.fd5c89fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Local-Weather-App/static/js/2.fd5c89fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf54ae682f8792a0e040",
+    "url": "/Local-Weather-App/static/js/main.ecead4d1.chunk.js"
+  },
+  {
+    "revision": "a0dccd9ef61afc711cee",
+    "url": "/Local-Weather-App/static/js/runtime-main.b482fb4a.js"
+  }
+]);
